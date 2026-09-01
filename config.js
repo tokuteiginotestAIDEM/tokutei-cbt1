5e1e0e13d5f31e8b770ad229290088d84d93784a
aidem <bichthaotranthi8@gmail.com>
2026-09-01 09:36:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CBT_CONFIG = {
   // Dán URL Google Apps Script Web App vào đây sau khi deploy.
-  apiUrl: "https://script.google.com/macros/s/AKfycbzkgM0JyjrJ3RHVGe3Tg_KgZzuxDY9olKVh2yStF8vW4ikE-3jz5oeiU-Q9IAuc1q0owg/exec",
+  apiUrl: "https://script.google.com/macros/library/d/1CvExHsKAOGNvoA-wRB2ZkIeMLV7YuvKS4QtVL5kK2x3BiJZ85ddb2UnA/1",
   siteTitle: "飲食料品製造業 模擬試験",
   defaultDurationMinutes: 70
 };
